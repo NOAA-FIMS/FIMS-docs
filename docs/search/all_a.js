0a034bd5d157cab90c10ccaa0400143ed2767bae
Christine Stawitz <christine.stawitz@noaa.gov>
2022-06-08 16:39:50 +0000

    Docs: run Doxygen

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -4,7 +4,7 @@ var searchData=
   ['real_5ft_34',['real_t',['../structfims_1_1FIMSTraits.html#a9c392642234ad977d0f8a6ea0dcf7106',1,'fims::FIMSTraits']]],
   ['recruitmentbase_35',['RecruitmentBase',['../structfims_1_1RecruitmentBase.html',1,'fims::RecruitmentBase&lt; Type &gt;'],['../structfims_1_1RecruitmentBase.html#a284bcb2e5db9b033c68a3fd314bd1a65',1,'fims::RecruitmentBase::RecruitmentBase()']]],
   ['recruitmentinterfacebase_36',['RecruitmentInterfaceBase',['../classRecruitmentInterfaceBase.html',1,'']]],
-  ['registerrandomeffect_37',['RegisterRandomEffect',['../classfims_1_1Information.html#a55f104ddbc63cca3637a69bf3ea6972b',1,'fims::Information']]],
-  ['registerrarameter_38',['RegisterRarameter',['../classfims_1_1Information.html#a2840d06e11b4847b1ae5fa7cfd22111b',1,'fims::Information']]],
+  ['registerparameter_37',['RegisterParameter',['../classfims_1_1Information.html#a7abb023dedff9942a54ddf65fa942fa9',1,'fims::Information']]],
+  ['registerrandomeffect_38',['RegisterRandomEffect',['../classfims_1_1Information.html#a55f104ddbc63cca3637a69bf3ea6972b',1,'fims::Information']]],
   ['rzero_39',['rzero',['../structfims_1_1SRBevertonHolt.html#a79cc0300e8d25b69e300924c9de9376c',1,'fims::SRBevertonHolt']]]
 ];
--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['recruitmentbase_79',['RecruitmentBase',['../structfims_1_1RecruitmentBase.html#a284bcb2e5db9b033c68a3fd314bd1a65',1,'fims::RecruitmentBase']]],
-  ['registerrandomeffect_80',['RegisterRandomEffect',['../classfims_1_1Information.html#a55f104ddbc63cca3637a69bf3ea6972b',1,'fims::Information']]],
-  ['registerrarameter_81',['RegisterRarameter',['../classfims_1_1Information.html#a2840d06e11b4847b1ae5fa7cfd22111b',1,'fims::Information']]]
+  ['registerparameter_80',['RegisterParameter',['../classfims_1_1Information.html#a7abb023dedff9942a54ddf65fa942fa9',1,'fims::Information']]],
+  ['registerrandomeffect_81',['RegisterRandomEffect',['../classfims_1_1Information.html#a55f104ddbc63cca3637a69bf3ea6972b',1,'fims::Information']]]
 ];
